50bce78516b0cc3783d5248e525fc58122c9d719
Singh <abhisheksingh75@deloitte.com>
2020-01-21 08:06:02 +0530

    2nd commit

--- router/main.js
+++ router/main.js
@@ -1,22 +1,17 @@
-module.exports = function(app)
-{
-  app.get('/',function(req,res){
+
+const express = require('express');
+let router = express.Router();
+
+  router.get('/',function(req,res){
     res.render('index.html');
     /*res.sendFile( __dirname + '../' + "index.html" )*/
     /*res.attachment('C:/Users/abhisheksingh75/Desktop/Acadmey/Code/express_1/views/002.jpg');*/
   });
-  app.get('/about',function(req, res){
-    res.render('about.html');
-  });
 
-  app.post('/check', function(req, res) {
-  console.log(req.body.username);
-  res.send("success");
-  var result = True;
-  	if(result){
-  		res.send("success");
-  	} else {
-  		res.send("failed");
-  	}
+  router.post('/check', function(req, res) {
+  //(req.body)
+  console.log(req.body);
+  //res.send(req.body);
+  res.send("success")
   });
-}
+  module.exports = router;
